56aec84f897e4f100b5b214893c54be36cab96cd
yxk <yuexiaokai@outlook.com>
2020-12-08 11:37:00 +0800

    跟新图片转换工具

--- Tools/figureFMT_convert/figureFMT_convert.py
+++ Tools/figureFMT_convert/figureFMT_convert.py
@@ -12,21 +12,16 @@ for n_line in range( len(line_vec)):
     #print(line[-1])
     #开头添加 <div align=center>
     if ('<img ' in line): 
-        if ('center' in line): continue
-        else:
-            line = '<div align=center>'+ line
+        if (not 'center' in line): line = '<div align=center>'+ line
     #末尾添加</div>
     if ('/>' in line):
-        if ('</div>' in line): continue
-        else:
-            line = line.replace('\n','') + '</div>'+'\n'
+        if (not '</div>' in line): line = line.replace('\n','') + '</div>'+'\n'
     #修改图片缩放格式
     if ('style="zoom:' in line):
         pos_start = line.index('style="zoom')
         pos_end   = line.index('%;"')+3
         style_str = line[pos_start:pos_end]
         zoom_ratio= float(style_str[12:-3])
-        #line.replace(style_str, 'width="%.1f%'%zoom_ratio)
         line=line.replace(style_str, 'width="%.1f'%zoom_ratio+'%"')
     #修改line_vec
     line_vec[n_line] = line
