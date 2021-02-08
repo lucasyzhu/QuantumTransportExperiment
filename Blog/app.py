eacf97de17d7fcb7a9052711d12b32655ec68abd
Kaige213 <yuexiaokai@outlook.com>
2021-02-08 16:33:47 +0800

    用python实现md->html->服务器，自动化

--- Blog/app.py
+++ Blog/app.py
@@ -4,39 +4,50 @@ from flask import render_template_string
 import os
 import time
 
-#查看所有的静态html文件，并存为html_files list
-html_files = []
-for root, dirs, files in os.walk("templates"):
-  for name in files:
-    html_files.append(os.path.join(root, name).replace('templates',''))
-    
-#----------------------------------------------
+
+#===============================================================================  
+def record(html_fileName, ip):
+  #Notes:
+  #  日志记录格式为：访问时间 | 访问者ip（实际为区域运营商的ip） | 访问的文件名 
+  #  ip的长度固定为15位，不足的话就在右边补空格
+  localtime = time.asctime( time.localtime(time.time()) )
+  data_str = "%s | %s | %s\n"%(localtime, ip.ljust(15,' '), html_fileName)
+  try:
+    with open("./log/record.txt", "a") as file:
+      file.write(data_str) 
+  except:
+    with open("./log/record.txt", "w") as file:
+      file.write(data_str) 
+
+#===============================================================================
 app = Flask(__name__)
-    
+
+#===============================================================================
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index/index.html')
 
+#===============================================================================
 @app.route('/<path:dummy>')
 def all_subpage(dummy):
-    #有时浏览器会在url末尾自动添加'/'，所以需要检查
-    url_str = request.path
-    if url_str.endswith('/'): url_str = url_str[:-1]
+    #找到html的文件名
+    for html_fileName in dummy.split('/'):
+      if ('.html' in html_fileName): break
 
-    #判断是否有网页文件
+    #查看所有的静态html文件
+    #-------------------------------------------
     #如果有，则返回相应静态网页，并记录时间，ip，以及网页
+    #Notes: 这里记录的应该是访问者使用的网络运营商的ip
+    for root, dirs, files in os.walk("templates"):
+      for name in files:
+        if (name == html_fileName): 
+          path_and_file = os.path.join(root, name).replace('templates','')
+          record(html_fileName,  request.remote_addr)
+          return render_template(path_and_file)
+    #-------------------------------------------
     #如果没有，则返回404页面
-    if (url_str in html_files):
-        record(url_str,  request.remote_addr)
-        return render_template(url_str)
     return render_template_string('<center><h1> 欧欧 </h1><h2>啥也没有</h2>')
     
-    
-def record(url_str, ip):
-  localtime = time.asctime( time.localtime(time.time()) )
-  data_str = "%s | %s | %s\n"%(localtime, ip, url_str)
-  with open("record.txt", "a") as file:
-    file.write(data_str) 
-  
+#=============================================================================== 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)#80, debug=True )
